75345d0afb6f132c5946bb5e6175cdd943057b34
Hassan Mohamed <HassanMohamed_HM@hotmail.com>
2022-06-09 15:27:14 +0200

    Styling

--- Blog/ViewModels/LoginViewModel.cs
+++ Blog/ViewModels/LoginViewModel.cs
@@ -1,7 +1,10 @@
-﻿namespace Blog.ViewModels;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.ViewModels;
 
 public class LoginViewModel
 {
     public string UserName { get; set; }
+    [DataType(DataType.Password)]
     public string Password { get; set; }
 }
