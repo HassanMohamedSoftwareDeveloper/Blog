aacbd936766f7b769745e4ca4fbbc8e291e2fb26
Hassan Mohamed <HassanMohamed_HM@hotmail.com>
2022-06-09 11:25:07 +0200

    Add Login & Logout

---
+++ Blog/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using Blog.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers;
+
+public class AuthController : Controller
+{
+    private readonly SignInManager<IdentityUser> _signInManager;
+
+    public AuthController(SignInManager<IdentityUser> signInManager)
+    {
+        _signInManager = signInManager;
+    }
+
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View(new LoginViewModel());
+    }
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginViewModel login)
+    {
+        var result = await _signInManager.PasswordSignInAsync(login.UserName, login.Password, false, false);
+        return RedirectToAction("Index", "Home");
+    }
+    public async Task<IActionResult> Logout()
+    {
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Home");
+    }
+}
