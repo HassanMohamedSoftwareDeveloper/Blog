bb62fde72cc399c2cf48186bbc968d4d566b4e77
Hassan Mohamed <HassanMohamed_HM@hotmail.com>
2022-06-13 14:34:25 +0200

    Fix Pagination Issue

--- Blog/Data/Repositories/Repository.cs
+++ Blog/Data/Repositories/Repository.cs
@@ -1,4 +1,5 @@
-﻿using Blog.Models;
+﻿using Blog.Helper;
+using Blog.Models;
 using Blog.Models.Comments;
 using Blog.ViewModels;
 using Microsoft.EntityFrameworkCore;
@@ -38,36 +39,12 @@ public class Repository : IRepository
             PageNumber = pageNumber,
             PageCount = pageCount,
             NextPage = postsCount > capacity,
-            Pages = PageNumbers(pageNumber, pageCount),
+            Pages = PageHelper.PageNumbers(pageNumber, pageCount).ToList(),
             Category = category,
             Posts = query.Skip(skipAmount).Take(pageSize).ToList(),
         };
     }
-    private static IEnumerable<int> PageNumbers(int pageNumber, int pageCount)
-    {
-        int midPoint = pageNumber < 3 ? 3 :
-            pageNumber > pageCount - 2 ? pageCount - 2 : pageNumber;
 
-        int lowerBound = midPoint - 2;
-        int upperBound = midPoint + 2;
-
-        if (lowerBound != 1)
-        {
-            yield return 1;
-            if (lowerBound - 1 > 1)
-                yield return -1;
-        }
-
-        for (int i = midPoint - 2; i <= midPoint + 2; i++)
-            yield return i;
-
-        if (upperBound != pageCount)
-        {
-            if (pageCount - upperBound > 1)
-                yield return -1;
-            yield return pageCount;
-        }
-    }
     public void AddPost(Post post) => _ctx.Posts.Add(post);
     public void UpdatePost(Post post) => _ctx.Update(post);
     public void RemovePost(int id) => _ctx.Posts.Remove(GetPost(id));
