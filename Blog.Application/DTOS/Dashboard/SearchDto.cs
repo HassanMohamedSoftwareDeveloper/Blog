f89090798c5bc40efa9ccf4ac9b27c2ad486413f
Hassan Mohamed <HassanMohamed_HM@hotmail.com>
2022-07-04 22:33:25 +0200

    Begin Search

---
+++ Blog.Application/DTOS/Dashboard/SearchDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Blog.Application.DTOS.Dashboard;
+
+public class SearchDto
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Image { get; set; }
+}
--- Blog.Portal/Pages/Index.cshtml.cs
+++ Blog.Portal/Pages/Index.cshtml.cs
@@ -1,20 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blog.Application.Queries.Dashboard;
+using Blog.Portal.ViewModels;
+using MediatR;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Blog.Portal.Pages
+namespace Blog.Portal.Pages;
+
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+    #region Fields :
+    private readonly IMediator _mediator;
+    #endregion
+
+    #region CTORS :
+    public IndexModel(IMediator mediator)
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
-
-        }
+        _mediator = mediator;
     }
+    #endregion
+
+    #region PROPS 
+    public SearchResultViewModel SearchResult { get; set; } = new();
+    public string Search { get; set; }
+    #endregion
+
+    #region Actions :
+    public async Task OnGet(string search)
+    {
+        this.Search = search;
+        SearchResult.LatestPosts = await _mediator.Send(new GetLatestPosts());
+        SearchResult.Categories = await _mediator.Send(new GetCategories());
+        SearchResult.Tags = await _mediator.Send(new GetTags());
+    }
+    #endregion
 }
